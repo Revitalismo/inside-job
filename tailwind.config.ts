36930c44fbbe2fe5db87e568880b71676049767d
Dev Julian <dev.achmadjulian@gmail.com>
2024-01-27 20:56:01 +0700

    feat: create Header component and setup colors in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,28 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      "primary": {
+        "0": "#D1EEFA",
+        "100": "#A2DCF5",
+        "200": "#5DC2EE",
+        "300": "#18A9E7",
+        "400": "#1387B9",
+        "500": "#0E658B",
+        "600": "#0A435C",
+        "700": "#05222E"
+      },
+      "neutral": {
+        "0": "#FFFFFF",
+        "100": "#F2F2F2",
+        "200": "#CCCCCC",
+        "300": "#B3B3B3",
+        "400": "#999999",
+        "500": "#4F4F4F",
+        "600": "#363636",
+        "700": "#1C1C1C",
+      }
+    }
   },
   plugins: [],
 };
