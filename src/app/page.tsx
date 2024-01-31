1c9a19020e6761ed231fc4e6217eae5b0bce0e2f
Celia Aulia <dev.celiaaulia@outlook.com>
2024-01-31 19:10:56 +0700

    feat: add Company Description section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import insideJobScene from "@/pictures/Inside Job Scene.webp";
+
 import pilahPilihMobile from "@/pictures/mockup/Homepage - Pilahpilih Mobile.webp";
 import pilahPilihDesktop from "@/pictures/mockup/Homepage - Pilahpilih Desktop.webp";
 
@@ -6,11 +8,13 @@ import Image from "next/image";
 import { Button } from "./components/Button";
 import { Collaboration } from "./components/Collaboration";
 import { Awards } from "./components/Awards";
+import { TeamCard } from "./components/TeamCard";
 
 export default function Home() {
   return (
     <main className="px-4 lg:px-12 bg-primary-700">
       <HeroSection />
+      <CompanyDescription />
     </main>
   );
 }
@@ -56,6 +60,47 @@ function HeroSection() {
           totalDeployedWeb={13}
         />
       </section>
+    </section>
+  );
+}
+
+function CompanyDescription() {
+  return (
+    <section className="flex flex-col gap-8 xl:flex-row items-center">
+      <section className="w-full lg:max-w-[800px] lg:min-w-[600px] h-[300px] relative">
+        <Image
+          alt="Inside Job Scene"
+          src={insideJobScene}
+          className="h-full object-cover"
+        />
+
+        <TeamCard 
+          name="Achmad Julian"
+          role="UI/UX Designer"
+        />
+
+        <TeamCard 
+          name="Weli Sagita"
+          role="Web Developer"
+        />
+
+        <TeamCard 
+          name="Muhamad Faisal"
+          role="Mobile Developer"
+        />
+
+        <TeamCard 
+          name="Bujangkuu"
+          role="Product Development"
+        />
+      </section>
+
+      <article className="flex flex-col gap-y-1.5 lg:max-w-[800px]">
+        <h1 className="text-[25px] sm:text-[39px] text-neutral-0 text-center xl:text-left font-medium">Inside Job</h1>
+        <p className="text:sm sm:text-base sm:leading-8 text-primary-0 text-justify xl:text-left leading-7">
+          Inside Job Merupakan sebuah Kolektif yang bergerak di bidang penyedia layanan Pembuatan website, jasa  SEO, Design UI/UX, Migrasi Website dan lain-lain. Kami berkomitmen untuk memberikan pelayanan dan dukungan yang luar biasa supaya situs web Anda lebih menonjol dari pesaing dan membantu bisnis Anda lebih berkembang.
+        </p>
+      </article>
     </section>
   );
 }
