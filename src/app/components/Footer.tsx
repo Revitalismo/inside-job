3feeff4c736aba75480b69d0d1dc57cea4f4b816
Dev Julian <dev.achmadjulian@gmail.com>
2024-02-01 01:41:59 +0700

    fix: add utility classes bg and text size

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 export function Footer() {
     return (
-        <footer className="py-8 px-4 md:px-12 min-h-[450px] w-full bg-primary-700 grid grid-flow-col justify-self-end gap-x-6 gap-y-8 border-b-[1.5px] border-primary-600 bg-primary-600 text-sm sm:text-base">
+        <footer className="py-8 px-4 md:px-12 min-h-[450px] w-full grid grid-flow-col justify-self-end gap-x-6 gap-y-8 bg-primary-600 border-t-[1.5px] border-primary-600 bg-primary-600 text-sm sm:text-base">
             
             <section className="flex flex-col gap-y-4 row-start-1 row-end-1 col-start-1 col-end-1">
                 <Image 
@@ -50,7 +50,7 @@ function Menu() {
         { name: ["/contact-us", "Kontak"] }
     ];
     return (
-        <article className="w-fit min-w-[90px] max-w-[600px] flex flex-col gap-y-3 col-start-2 col-end-2 justify-self-end text-primary-0">
+        <article className="w-fit min-w-[90px] max-w-[600px] flex flex-col gap-y-3 col-start-2 col-end-2 justify-self-end text-primary-0]">
             <h4 className="font-medium text-neutral-0">Menu utama</h4>
 
             <section className="flex flex-col gap-y-2.5 row-span-2 col-span-1">
@@ -79,14 +79,14 @@ function Services() {
         { name: ["/quality-asurance", "Quality Asurance"] }
     ];
     return (
-        <article className="w-fit min-w-[150px] max-w-[600px] row-start-2 row-end-2 col-span-1 justify-self-start flex flex-col gap-y-3 sm:col-start-3 sm:col-end-3 sm:row-start-1 sm:row-end-1 sm:justify-self-end text-primary-0">
+        <article className="w-fit min-w-[150px] max-w-[600px] row-start-2 row-end-2 col-span-1 justify-self-start flex flex-col gap-y-3 sm:col-start-3 sm:col-end-3 sm:row-start-1 sm:row-end-1 sm:justify-self-end">
             <h4 className="font-medium text-neutral-0">Layanan kami</h4>
 
             <section className="flex flex-col gap-y-2.5">
                 {
                     serviceMap.map((menu) => {
                         return (
-                            <Link href={menu.name[0]} className="hover:text-primary-200" key={menu.name[0]}>
+                            <Link href={menu.name[0]} className="text-primary-0 hover:text-primary-200" key={menu.name[0]}>
                                 {menu.name[1]}
                             </Link>
                         );
