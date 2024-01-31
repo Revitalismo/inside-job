f15c0d87cfdb91bb09f1b3a03a0772dc206f803a
Dev Julian <dev.achmadjulian@gmail.com>
2024-01-31 22:24:11 +0700

    fix: add utility class line-clamp-2 to create multiline text in line 2 if it overflows then add three dots behind it

--- src/app/components/ServiceCard.tsx
+++ src/app/components/ServiceCard.tsx
@@ -13,14 +13,16 @@ export function ServiceCard({ title, description, navigateTo }: ServiceCardProps
     return (
         <Link
             href={navigateTo ? navigateTo : "/"}
-            className="w-full max-w-[328px] py-4 px-6 flex flex-col gap-y-4 bg-primary-600 border-[1.5px] border-primary-500 rounded-lg"
+            className="w-full max-w-[328px] sm:max-h-[200px] py-4 px-6 flex flex-col gap-y-4 bg-primary-600 border-[1.5px] border-primary-500 rounded-lg"
         >
             <article className="flex flex-col gap-y-1.5">
                 <h4 className="text-base sm:text-2xl font-medium text-neutral-0">{ title }</h4>
-                <p className="text-sm leading-6 sm:text-base sm:leading-8 text-primary-0">{ description }</p>
+                <p className="text-sm leading-6 sm:text-base sm:h-[70px] sm:leading-8 text-primary-0 line-clamp-2">
+                    { description }
+                </p>
             </article>
 
-            <section className="flex items-center gap-x-1.5">
+            <section className="w-full flex items-center gap-x-1.5">
                 <h4 className="text-sm sm:text-base text-medium text-primary-0">Lihat selengkapnya</h4>
                 
                 <Image 
